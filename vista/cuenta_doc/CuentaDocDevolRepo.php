b1c13e69c01c829b4527c0dca312f9490aeedd33
mguerra <manuel.guerra@endetransmision.bo>
2020-10-06 10:28:21 -0400

    #ETR-1248  eliminar caracter especial

--- vista/cuenta_doc/CuentaDocDevolRepo.php
+++ vista/cuenta_doc/CuentaDocDevolRepo.php
@@ -7,14 +7,21 @@
 *@description Archivo con la interfaz de usuario que permite 
 *procesar las devoluciones/reposiciones
 *
+#HISTORIAL DE MODIFICACIONES:
+#ISSUE          FECHA        AUTOR              DESCRIPCION
+#ETR-1248      06/10/2020     manuelg guerra     eliminar caracter especial
+
+
 */
+
+
 header("content-type: text/javascript; charset=UTF-8");
 ?>
 <script>
 Phx.vista.CuentaDocDevolRepo = {
 	require:'../../../sis_cuenta_documentada/vista/cuenta_doc/CuentaDocVbContaCentral.php',
 	requireclase:'Phx.vista.CuentaDocVbContaCentral',
-	title:'Cuenta Documentada/Devoluciones',
+	title:'Cuenta Documentada Devoluciones',
 	nombreVista: 'CuentaDocDevolRepo',
 	
 	constructor: function(config){
