a27792d62b8b423fe9a082af4b65c4094fd814b4
mguerra <manuel.guerra@endetransmision.bo>
2020-11-30 14:12:53 -0400

    ETR-1991

--- base/dataupdate000001.sql
+++ base/dataupdate000001.sql
@@ -1,3 +1,27 @@
 /********************************************I-DAUP-AUTOR-SCHEMA-0-31/02/2019********************************************/
 --SHEMA : Esquema (CONTA) contabilidad         AUTHOR:Siglas del autor de los scripts' dataupdate000001.txt
 /********************************************F-DAUP-AUTOR-SCHEMA-0-31/02/2019********************************************/
+
+
+
+
+/********************************************I-DAUP-AUTOR-SCHEMA-0-31/02/2019********************************************/
+--SHEMA : Esquema (CONTA) contabilidad         AUTHOR:Siglas del autor de los scripts' dataupdate000001.txt
+--rollback
+--begin;
+--UPDATE cd.tcuenta_doc SET estado='rendido' and tipo_rendicion='parcial' WHERE id_cuenta_doc=30764;
+--commit;
+
+BEGIN;
+UPDATE cd.tcuenta_doc SET estado='vbtesoreria' and tipo_rendicion='final' WHERE id_cuenta_doc=30764;
+COMMIT;
+
+--rollback
+--begin;
+--UPDATE wf.testado_wf SET id_tipo_estado=664 WHERE id_estado_wf=1162541;
+--commit;
+
+BEGIN;
+UPDATE wf.testado_wf SET id_tipo_estado=695 WHERE id_estado_wf=1162541;
+COMMIT;
+/********************************************F-DAUP-AUTOR-SCHEMA-0-31/02/2019********************************************/
